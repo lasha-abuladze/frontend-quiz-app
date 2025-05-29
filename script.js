2a8c7161fd11f6221421f1954cf8e13b5dc89538
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2025-05-30 00:45:58 +0400

    start working on a project, create main section and main header. also add theme switcher funtionality

---
+++ script.js
@@ -0,0 +1,26 @@
+`use strict`;
+
+const body = document.querySelector(`body`);
+const switcherIcon = document.querySelector(`.switcher-icon`);
+const switcherCircle = document.querySelector(`.switcher-circle`);
+const themeSwitcherIcons = document.querySelectorAll(`.theme-switcher--icon`);
+const themeSwitchers = document.querySelectorAll(`.switch-theme`);
+
+
+
+themeSwitchers.forEach(switchTheme);
+
+
+
+
+/// switchTheme function swotchs theme from light to dark and vice verca;
+function switchTheme (el) {
+    el.addEventListener(`click`, () => {
+        switcherCircle.classList.toggle(`dark-switcher`);
+        themeSwitcherIcons.forEach(el => {
+            el.classList.toggle(`display-none`)
+        })
+        body.classList.toggle(`body-dark`)
+    })
+}
+
