fc030f582d8fea3635700513bdb3e4ab694e9e39
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2025-06-15 12:26:52 +0400

    add functions that displays quiz question and its answers

--- script.js
+++ script.js
@@ -13,15 +13,23 @@ const ind = document.querySelectorAll(`.ind`);
 const sections = document.querySelectorAll(`.section`);
 const quizTopicsListBtn = document.querySelectorAll(`.quiz-topics-list--li`);
 
+const quizHeader = document.querySelector(`.quiz-header`);
+const answersList = document.querySelector(`.answers-list`);
+
+
+let questionNumber = 0;
+let answerIndex = ``;
 
 let quizTopic = ``;
-
+let quiz;
 
 themeSwitchers.forEach(switchTheme);
 
 quizTopicsListBtn.forEach(chooseQuiz);
 
-/// switchTheme function swotchs theme from light to dark and vice verca;
+
+
+/// switchTheme function switchs theme from light to dark and vice verca;
 function switchTheme (el) {
     el.addEventListener(`click`, () => {
         switcherCircle.classList.toggle(`dark-switcher`);
@@ -34,17 +42,92 @@ function switchTheme (el) {
     })
 }
 
-
+/// shooseQuiz functions changes sections and displaies chosen quiz
 function chooseQuiz (el) {
 
     el.addEventListener(`click`, () => {
+        questionNumber++;
         quizTopic = el.querySelector(`.quiz-topic`).textContent.toUpperCase();
-        console.log(quizTopic)
         sections.forEach(el => el.classList.toggle(`display-none`))
+        displayQuiz();
     })
 }
 
 
+
+
+function displayQuiz () {
+
+    fetch(`./data.json`).then(res => res.json()).then(data => {
+
+        data.quizzes.forEach((el, i) => {            
+            if(quizTopic === el.title.toUpperCase()) {
+                quiz = data.quizzes[i].questions;
+            }
+        })
+
+        displayQuestion(quiz);
+        displayAnswers(quiz);
+
+    })
+
+}
+
+
+
+function displayQuestion(quiz) {
+
+    quizHeader.textContent = ``;
+    
+    const quizQuestionHTML = `
+                <p class="question-number">
+                    Question ${questionNumber} of ${quiz.length}
+                </p>
+                
+                <p class="question">
+                    ${quiz[questionNumber-1].question}
+                </p>
+    `
+
+    quizHeader.insertAdjacentHTML(`afterbegin`, quizQuestionHTML);
+}
+
+
+
+function displayAnswers(quiz) {
+    answersList.textContent = ``;
+
+    quiz[questionNumber-1].options.forEach((el, i)=> {
+        
+        if (i===0) {
+            answerIndex = `d`
+        } else if (i===1) {
+            answerIndex = `c`
+        } else if (i===2) {
+            answerIndex = `b`
+        } else if (i===3) {
+            answerIndex = `a`
+        }
+
+        const answerHTML = `
+            <li class="answers-list--li">
+                <input type="radio" id="${i}" name="answer">
+                <label class="answers-list--label" for="${i}">
+                    <p>
+                        <b class="answer-number">
+                            ${answerIndex}
+                        </b>
+                        ${el}
+                    </p>
+                </label>
+            </li>    
+        `
+
+        answersList.insertAdjacentHTML(`afterbegin`, answerHTML);
+
+    })
+
+}
 
 
 
