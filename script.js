58a6768925b7ec99d29d11b52e6817cbf4374b65
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2025-06-30 21:18:38 +0400

    implement themeSwitcher functionality

---
+++ script.js
@@ -0,0 +1,25 @@
+`use strict`;
+
+const body = document.querySelector(`body`);
+const themeSwitcherBtn = document.querySelector(`.div--theme-switcher`);
+const themeSwitcherIcons = document.querySelectorAll(`.theme-switcher--icon`);
+let subTittle = document.querySelector(`.subtittle`);
+let listCell = document.querySelectorAll(`.quiz-list--li`);
+
+
+
+// Toggle between light and dark themes when the themeSwitcher button is clicked.
+// - Toggles 'body-dark' class on the body element
+// - Shows/hides theme switcher icons by toggling 'display-none' class
+// - Changes subtitle style by toggling 'subtittle-dark' class
+// - Updates each list cell's style by toggling 'listcell-dark' class
+
+themeSwitcherBtn.addEventListener(`click`, () => switchTheme());
+
+
+function switchTheme() {
+    body.classList.toggle(`body-dark`);
+    themeSwitcherIcons.forEach(el => el.classList.toggle(`display-none`));
+    subTittle.classList.toggle(`subtittle-dark`);
+    listCell.forEach(el => el.classList.toggle(`listcell-dark`));
+}
